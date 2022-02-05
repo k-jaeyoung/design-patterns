2739b4739a065f329937020d0f1df228063150bc
jaeyoungko <dungsil2@uniess.co.kr>
2022-02-05 23:25:21 +0900

    두번째 싱글톤

---
+++ src/singleton/Settings_Final.java
@@ -0,0 +1,14 @@
+package singleton;
+
+public class Settings_Final {
+
+    private Settings_Final() {}
+
+    private static class SettingHolder {
+        private static final Settings_Final INSTANCE = new Settings_Final();
+    }
+
+    public static Settings_Final getInstance() {
+        return SettingHolder.INSTANCE;
+    }
+}
